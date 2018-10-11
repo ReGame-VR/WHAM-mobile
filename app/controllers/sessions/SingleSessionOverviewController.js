30defa71fabc5c2fba14afc1b60af97c0ea401e7
Ryan Bradford <ryanbradford333@gmail.com>
2018-10-11 12:28:07 -0400

    Changed the overview page just slightly

--- app/controllers/sessions/SingleSessionOverviewController.js
+++ app/controllers/sessions/SingleSessionOverviewController.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback} from 'react-native';
 
 export default class SingleSessionOverviewController extends React.Component {
     
     constructor(props) {
         super(props)
     }
-
+    
+    // Button  might remain centered because the width is 100% and the title is centered
     render() {
-        return <View style={styles.container}>
-                <Button onPress={this.props.action} title="More Info"
-                style={{position: "absolute", flex: 1, right: 0}}></Button>
-                <Text style={{position: "absolute", flex: 1, left: 0}}>
-                {this.props.session.get_average_score()}</Text>
-                </View>
+        return (
+        <View style={styles.container}>
+            <Text style={{position: "relative", flex: 1}}>{this.props.session.get_average_score()}</Text>
+            <TouchableWithoutFeedback onPress={this.props.action}
+                style={{flex: 1, position: "relative"}}>
+                <View><Text>Press Me!</Text></View>
+            </TouchableWithoutFeedback>
+        </View>
+        )
     }
 
 }
 
 const styles = StyleSheet.create({
     container: {
-      justifyContent: 'center',
-      backgroundColor: "green"
+        flexDirection: "row",
+        width: "100%",
+        backgroundColor: "green"
     },
   });
